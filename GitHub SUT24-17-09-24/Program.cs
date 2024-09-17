13da472e1b87a480c51a7c537ecc2682cedd4540
FelloMan <filip0816@hotmail.com>
2024-09-17 10:11:21 +0200

    Add project files.

---
+++ GitHub SUT24-17-09-24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHub_SUT24_17_09_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
